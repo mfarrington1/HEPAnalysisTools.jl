2eaaa6d20c70bfb982bc2cdc9b969e1f9c2bc317
mfarring <michael.steven.farrington@cern.ch>
2025-04-29 15:05:58 -0500

    Included event_display so it could be used.

--- src/HEPAnalysisTools.jl
+++ src/HEPAnalysisTools.jl
@@ -10,4 +10,7 @@ export pdf_plot, plot_hist, plot_comparison, multi_plot
 include("./PlottingObjects.jl")
 export gaudi_colors, AtlasTheme, set_ATLAS_theme, add_ATLAS_internal!
 
+include("./EventDisplay.jl")
+export event_display
+
 end
