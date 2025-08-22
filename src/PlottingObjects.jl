34535498fa84bb388a5a4505dda0fe6188fffd92
mfarring <michael.steven.farrington@cern.ch>
2025-08-22 13:02:34 -0500

    Fixed issue with sqrt{s} not showing up.

--- src/PlottingObjects.jl
+++ src/PlottingObjects.jl
@@ -34,7 +34,7 @@ function add_ATLAS_internal!(ax, sec_text; offset=(250, -20), fontsize=20)
         align=(:left, :top), offset, space=:relative
     )
 
-    text!(ax, 0, 1; text=L"$\mathbf{\sqrt{s}}$", align=(:left, :top), offset=(offset[1], offset[2] - 30), space=:relative, fontsize=15)
+    text!(ax, 0, 1; text=L"$\fontfamily{NewComputerModern}\mathbf{\sqrt{s}}$", align=(:left, :top), offset=(offset[1], offset[2] - 30), space=:relative, fontsize=15)
     text!(ax, 0, 1; text=L"\fontfamily{TeXGyreHeros} $= 13$ TeV", align=(:left, :top), offset=(offset[1]+25, offset[2] - 30), space=:relative, fontsize=15)
 
     nothing
